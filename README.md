83bde067188f0f05359def3fda49fcb9d995c679
Yorick Peterse <yorick@yorickpeterse.com>
2026-03-19 16:09:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ping-monitor
+
+A Docker/Podman container for monitoring ping timings.
+
+## Requirements
+
+- Docker/Podman
+
+## License
+
+All source code in this repository is licensed under the Mozilla Public License
+version 2.0, unless stated otherwise. A copy of this license is found in the
+file "LICENSE".
---
+++ monitor.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+if ! test -n "$DB_IP"
+    echo 'The DB_IP variable must be set'
+    exit 1
+end
+
+if ! test -n "$DB_PORT"
+    echo 'The DB_PORT variable must be set'
+    exit 1
+end
+
+while true
+    sleep (random 180 300)
+
+    for host in $argv
+        set msec (ping -q -c 5 $host | tail -n 1 | cut -d = -f 2 | cut -d / -f 2)
+        set time (math "$msec / 1000")
+
+        if test $status -eq 0
+            echo "ping_times,host=$host sum=$time" | ncat --udp $DB_IP $DB_PORT
+            echo "$host: $time seconds"
+        end
+    end
+end
